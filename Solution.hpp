c02c323848c02ba9c1a319b91e41a19723dcc753
Alessandro Nicolì <38593853+Niloqui@users.noreply.github.com>
2019-11-28 01:08:27 +0100

    Solution Update 3

--- Solution.hpp
+++ Solution.hpp
@@ -20,7 +20,11 @@ public:
 	int* indexexams;
 	bool* mask;
 	// indexexams e mask possono essere utilizzati in un sottografo per contenere la soluzione di quel sottografo
-	// Non vengono mai usati direttamente in
+	bool optimum;
+	// Questa variabile ha più senso per le soluzioni ai sottografi che all'intero grafo
+	// Se la dimensione del sottografo è piccola (circa 6/7 nodi), è possibile eseguire un algoritmo esatto
+	// La sottosoluzione trovata da un algoritmo esatto avrà optimum = true
+	// In caso di optimum = true, non si eseguiranno nuovi algoritmi per tentare di migliorare la soluzione trovata
 
 	Solution(int n, int tmax); // Costruttore
 	Solution(int n, int tmax, int* newsol); // Costruttore con soluzione iniziale
@@ -36,9 +40,12 @@ public:
 	int calculatePenalty(G::Graph g, int* indexvector, int nsub);
 	// Tutte le funzioni calculatePenalty restituiscono -1 se la soluzione è unfeasible
 
+	// (forse) TO-DO: provare a invertire la seconda e la terza calculatePenalty, ovvero la terza sarà un wrapper della seconda.
+	// Controllare poi quale delle due versioni è più efficiente
+
 	void printSolution(std::string filename); // Esporta la soluzione su file
 
-	~Solution() { delete[] sol; }
+	~Solution() { delete[] sol; delete[] mask; delete[] indexexams; }
 
 
 };
